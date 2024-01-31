4a75cc2fd245fe562227782bcf28e9a08aabe0e3
tejaskatkade <105963561+tejaskatkade@users.noreply.github.com>
2024-01-31 22:32:26 +0530

    file upload

---
+++ restapi/restapiUsingDB/src/main/java/com/tejas/restapi/controller/FileUploadController.java
@@ -0,0 +1,42 @@
+package com.tejas.restapi.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import com.tejas.restapi.helper.FileUploadHelper;
+
+@RestController
+public class FileUploadController {
+
+    @Autowired
+    private FileUploadHelper fileUploadHelper; 
+    
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("sinhgadimg") MultipartFile file){
+
+        //validation
+
+        System.out.println("OriginalFilename : "+file.getOriginalFilename());
+        // System.out.println("ContentType : "+file.getContentType());
+        System.out.println("Name : "+file.getName());
+        System.out.println("Size : "+file.getSize());
+
+        if(file.isEmpty()){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File is Empty ...!");
+        }
+        
+        if(!file.getContentType().equals("image/jpeg")){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Not a image/jpeg");
+        }
+
+        // Uploading file
+        boolean val = fileUploadHelper.uploadFile(file);
+        if(val){
+            return ResponseEntity.ok("File Uploaded Successfully");
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong");
+    } 
+}
---
+++ restapi/restapiUsingDB/src/main/java/com/tejas/restapi/helper/FileUploadHelper.java
@@ -0,0 +1,52 @@
+package com.tejas.restapi.helper;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MultipartFile;
+
+//import java.io.FileOutputStream;
+//import java.io.InputStream;
+
+@Controller
+public class FileUploadHelper {
+    // Path where we want to store data
+    public final String UPLOAD_DIR = "D:\\Code\\springboot\\restapi\\restapiUsingDB\\src\\main\\resources\\static\\image";
+    
+    
+    public boolean uploadFile(MultipartFile multipartfFile){
+    
+        boolean val = false;
+        
+    //     // way- 1
+        
+    //     // reading data
+    //    try {
+    //     InputStream is = multipartfFile.getInputStream();
+    //     byte data[] = new byte[is.available()];
+    //     is.read(data);
+
+    //     //write
+    //     FileOutputStream fos = new FileOutputStream(UPLOAD_DIR + File.separator + multipartfFile.getOriginalFilename());
+    //     fos.write(data);
+    //     fos.flush();
+    //     fos.close();
+    //     val = true;
+
+    //    } catch (Exception e) {
+    //         e.printStackTrace();
+    //    }
+
+       //way-2
+       try {
+
+        Files.copy(multipartfFile.getInputStream(),Paths.get(UPLOAD_DIR+File.separator+multipartfFile.getOriginalFilename()),StandardCopyOption.REPLACE_EXISTING);
+        val = true;
+       } catch (Exception e) {
+        e.printStackTrace();
+       }
+        return val;
+    }
+}
